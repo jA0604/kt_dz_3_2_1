93bfeaf37744c55bed2bbaf036505dc5834e93d9
jA0604 <j1107@bk.ru>
2021-01-04 17:42:41 +0300

    edit main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,3 @@
-import model.Note
-import service.NoteService
-
 fun main() {
 
 }
-//
-//fun noteService (): Note? {
-//    val ns = NoteService()
-//    val note = Note(id = 0, ownerIid = 0, title = "Title 1", text = "Test text on Note 1",
-//        date = 1609879870328798L, comments = 1, readComments = 0, viewUrl = "url_note")
-//    ns.add(note)
-//    val d = ns.getById(0)
-//    return d
-////    println(d)
-//}
